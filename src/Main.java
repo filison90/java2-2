4d719ce943fffd79fe2fbd6b7f47265578fbde11
Филимонов Максим <filison90@gmail.com>
2022-06-02 22:58:09 +0300

    отформатировал кодÐ

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
 
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
 
-                // Объявляете переменные для входных данных и
-                // параметров программы: начального счёта,
-                // суммы пополнения и тп
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
 
-                // Условным оператором проверяете превысила ли
-                // сумма пополнения порог и для этих двух разных
-                // сценариев рассчитываете сумму бонуса и выводите
-                // на экран.
-
-        int replenishment  = 900;
+        int replenishment = 900;
         int bonus = 100;
         int remains = 50;
 
         boolean cost = replenishment > 1000;
 
-        if (cost){
-            System.out.println((replenishment/bonus)+remains+replenishment);
-        }else {
-            System.out.println(replenishment+remains);
+        if (cost) {
+            System.out.println((replenishment / bonus) + remains + replenishment);
+        } else {
+            System.out.println(replenishment + remains);
         }
 
     }
